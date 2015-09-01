a3a88f05ab86d47687679bac40a434af3d2359d8
catie <catiestallings1@gmail.com>
2015-09-01 15:10:59 -0500

    Added fruitBearing method

--- learn_to_program/orange_tree.rb
+++ learn_to_program/orange_tree.rb
@@ -2,16 +2,32 @@ class OrangeTree
 
 	def initialize
 		@height = 0
-		@age = 0
+		@age = 
+		@oranges = 0
 	end
 
 	def height
 		@height
 	end
 
+	def fruitBearing
+		if @age >= 0 && @age <= 3
+			@fruit = 0
+		elsif @age >= 4 && @age <=10
+			@fruit = rand(15..25)
+		else
+			@fruit = rand(26..35)
+		end
+		return puts "Your tree bore #{@fruit} oranges."
+	end
+
 	def oneYearPasses
 		@age = @age + 1
-		@height = @height + 3
+		@height = @height + 1.25
+		if @age > 20
+			puts "Your tree lived a long and healthy life; now it's dead."
+			exit
+		end
 		return print "age: #{@age} year(s), #{@height} ft"
 	end
 
@@ -21,6 +37,10 @@ end
 tree = OrangeTree.new
 
 
-puts tree.oneYearPasses
+20.times do puts tree.oneYearPasses
+end
+
+puts tree.fruitBearing
+
 
 
