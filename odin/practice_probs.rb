67aa4d107671e593651801427f7c266d5f42770b
catie <catiestallings1@gmail.com>
2015-10-21 13:14:06 -0500

    Practice Problems

---
+++ odin/practice_probs.rb
@@ -0,0 +1,137 @@
+# Fibonacci Sequence
+
+#0, 1, 1, 2, 3, 5, 8 ..etc
+
+
+# def fibs(n)
+
+# 	if n == 1 || n == 2
+# 			return 1
+# 	else
+# 		fibs(n - 1) + fibs(n - 2)
+# 	end
+
+# end
+
+# puts fibs(3)
+
+
+
+# def flatten(array, new_array = [])
+# 		array.each do |element|
+# 			if element.is_a?(Array)
+# 				flatten(element, new_array)
+# 			else
+# 				new_array << element
+# 			end
+
+# 		end
+
+# 		new_array
+
+# end
+
+# print flatten([[1, [8, 9]], [3, 4]])
+
+
+# def factorial(n)
+#   if n == 0
+#     1
+#   else
+#     n * factorial(n-1)
+#   end
+# end
+ 
+# puts factorial(2)
+
+# A palindromic number reads the same both ways. 
+# The largest palindrome made from the product of two 
+# 2-digit numbers is 9009 = 91 × 99.
+
+# Find the largest palindrome made from the product of two 3-digit numbers.
+
+
+
+def palindrome
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
