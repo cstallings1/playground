c4405bfb6b5773e1feca8bd4361fa8b8817e7bdf
cstallings1 <catiestallings1@gmail.com>
2015-11-03 10:46:30 -0600

    Created database and saving functionality

--- jumpstartlab/webapp_1/app.rb
+++ jumpstartlab/webapp_1/app.rb
@@ -1,8 +1,19 @@
 require 'sinatra'
 require 'sinatra/reloader'
-require 'sinatra/activerecord'
-require './config/enironments'
+require 'rubygems'
+require 'data_mapper'
 
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/development.db")
+
+class Order
+  include DataMapper::Resource
+  property :id,           Serial
+  property :name,         String, :required => true
+  property :drink, 		  String
+  property :temperature,  String
+  property :ordered_on, DateTime
+end
+DataMapper.finalize
 
 get '/' do 
 	@title = "Drink Order"
@@ -14,14 +25,19 @@ post '/order' do
 	@name = params["name"]
 	@drink = params["drink"] 
 	@temperature = params["temperature"]
+
+	Order.new(params).save!
 	
 	erb :order
 end
 
 get '/history' do 
 	@title = 'Today\'s Order History'
-	erb :history, :locals => {"name" => @name, "drink" => @drink}
+	@orders = Order.all
+
+	erb :history
 end
+
 
 
 	
